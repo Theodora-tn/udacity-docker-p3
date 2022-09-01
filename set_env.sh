95bcaa4659275e1ec5938420a8227b33d2728b40
thientnq <thien.tnq@gmail.com>
2022-09-01 12:50:20 +0700

    congif.ts change possgresS

--- set_env.sh
+++ set_env.sh
@@ -1,9 +1,9 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=adminthien
-export POSTGRES_PASSWORD=adminthien
-export POSTGRES_HOST=db-udacity-p3.c4ablgoirek5.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
+export POSTGRESS_USERNAME=adminthien
+export POSTGRESS_PASSWORD=adminthien
+export POSTGRESS_HOST=db-udacity-p3.c4ablgoirek5.us-east-1.rds.amazonaws.com
+export POSTGRESS_DB=postgres
 export AWS_BUCKET=thien-udacity-p3-bucket  
 export AWS_REGION=us-east-1
 export AWS_PROFILE=p3udacity
